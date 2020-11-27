13fc80ef56a756fcee5dca38a5976ab0aa3e5a62
Hirokazu TAKATAMA <hirokazu.takatama@gmail.com>
2020-11-27 16:24:09 +0900

    Step1の0の時にOK

--- warikan/warikan.py
+++ warikan/warikan.py
@@ -2,7 +2,7 @@ class Warikan:
 
     def calc(self, total_bill, members):
        
-        member_bill = -1 
+        member_bill = 0 
         kanji_bill = 0
         
         return member_bill, kanji_bill 
