0ef245f4272fb27efbcf57bb15ab022aaba4a9ca
ekrecker <eiichi.kendoruki@gmail.com>
2020-11-27 23:58:03 +0900

    test名をtotal_members_0に変更

--- tests/test_warikan.py
+++ tests/test_warikan.py
@@ -13,7 +13,7 @@ class TestWarikan(TestCase):
     def test_calc_kanji_only_normal_2yen(self):
         self.assertEqual(Warikan().calc(3330, 4, 0, 0), (833, 0, 0, 2))
 
-    def test_calc_kanji_only_normal_0yen(self):
+    def test_calc_kanji_only_normal_total_members_0(self):
         self.assertEqual(Warikan().calc(3330, 0, 0, 0), (0, 0, 0, 0))
 
     def test_calc_kanji_1yen(self):
