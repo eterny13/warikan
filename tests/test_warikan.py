e9c2cdf796153adb4e815e086002c111e13ec37f
ekrecker <eiichi.kendoruki@gmail.com>
2020-11-27 23:52:20 +0900

    step2全テストパス

--- tests/test_warikan.py
+++ tests/test_warikan.py
@@ -10,5 +10,11 @@ class TestWarikan(TestCase):
     def test_calc_normal_kanji_2yen(self):
         self.assertEqual(Warikan().calc_normal(3330, 4), (833, 2))
 
+    def test_calc_kanji_only_normal_2yen(self):
+        self.assertEqual(Warikan().calc(3330, 4, 0, 0), (833, 0, 0, 2))
+
+    def test_calc_kanji_only_normal_0yen(self):
+        self.assertEqual(Warikan().calc(3330, 0, 0, 0), (0, 0, 0, 0))
+
     def test_calc_kanji_1yen(self):
-        self.assertEqual(Warikan().calc(3330, 4, 0, 0), (833, 0, 0, 1))
+        self.assertEqual(Warikan().calc(3330, 2, 1, 1), (833, 1249, 416, 1))
--- warikan/warikan.py
+++ warikan/warikan.py
@@ -1,4 +1,5 @@
-from math import ceil
+from math import ceil, floor
+from decimal import Decimal, ROUND_HALF_UP
 
 
 class Warikan:
@@ -14,4 +15,29 @@ class Warikan:
         return member_bill, kanji_bill 
 
     def calc(self, total_bill, normal_members, large_members, small_members):
-        return (0, 0, 0, 0) 
+
+        ratio = small_members + normal_members * 2 + large_members * 3 
+
+        if ratio <= 0:
+            return (0, 0, 0, 0)
+        
+        bills = [(total_bill / ratio, small_members), (total_bill / ratio * 2, normal_members), (total_bill / ratio * 3, large_members)]
+
+        round_bills = []
+        kanji_bill = 0
+        for each_bill, each_members in bills: 
+
+            if each_members <= 0:
+                round_bills.append(0)
+
+            else:
+                if each_bill%1 < 0.5:
+                    round_bills.append(floor(each_bill))
+                    kanji_bill += floor(each_bill) * each_members
+                else:
+                    round_bills.append(floor(each_bill)+1)
+                    kanji_bill += (floor(each_bill)+1) * each_members
+
+        kanji_bill -= total_bill
+
+        return (round_bills[1], round_bills[2], round_bills[0], kanji_bill) 
