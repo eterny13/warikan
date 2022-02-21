11cbb2223fba74fae82165ca6beaf2d53217707b
kawagoo <eiichi.kendoruki@gmail.com>
2022-02-21 22:34:19 +0900

    Update calc10yen

--- Warikan.hs
+++ Warikan.hs
@@ -10,7 +10,7 @@ normalCalc n tb = [ch, mb]
     mb = tb `div` n + 1
     ch = mb * n - tb
 
-calc :: [Member] -> Totalbill -> [Integer]
+calc :: [Member] -> Totalbill -> [Int]
 calc ms tb = [ch, mb, lmb, smb] 
   where 
     nl = length $ filter (==NormalMember) ms 
@@ -18,19 +18,24 @@ calc ms tb = [ch, mb, lmb, smb]
     sl = length $ filter (==SmallMember) ms 
     n  = fromIntegral nl + fromIntegral ll * 1.5 + fromIntegral sl * 0.5 
 
-    mb  = ceiling $ fromIntegral tb / n 
+    mb  = round $ fromIntegral tb / n + 0.01
     lmb = truncate $ fromIntegral mb * 1.5 
     smb = truncate $ fromIntegral mb * 0.5  
     ch  = mb * fromIntegral nl + lmb * fromIntegral ll + smb * fromIntegral sl - fromIntegral tb 
 
-calc10yen :: Npeople -> Totalbill -> [Int]
-calc10yen n tb = [mb', lmb', smb']
+calc10yen :: [Member] -> Totalbill -> [Int]
+calc10yen ms tb = [mb', lmb', smb']
   where
-    mb = tb `div` n + 1
-    lmb = mb * 3 `div` 2 
-    smb = mb `div` 2
+    nl = length $ filter (==NormalMember) ms 
+    ll = length $ filter (==LargeMember) ms 
+    sl = length $ filter (==SmallMember) ms 
+    n  = fromIntegral nl + fromIntegral ll * 1.5 + fromIntegral sl * 0.5 
 
-    mb' = if mb `mod` 10 <= 4 then tb `div` n `div` 10 * 10 else tb `div` n `div` 10 * 10 + 10
+    mb  = round $ fromIntegral tb / n + 0.01
+    lmb = truncate $ fromIntegral mb * 1.5 
+    smb = truncate $ fromIntegral mb * 0.5  
+
+    mb'  = if  mb `mod` 10 <= 4 then  mb `div` 10 * 10 else  mb `div` 10 * 10 + 10
     lmb' = if lmb `mod` 10 <= 4 then lmb `div` 10 * 10 else lmb `div` 10 * 10 + 10
     smb' = if smb `mod` 10 <= 4 then smb `div` 10 * 10 else smb `div` 10 * 10 + 10
 
@@ -39,8 +44,8 @@ main = do
   let s1 = normalCalc 4 3330
   print s1
   
-  let s2 = calc [NormalMember, NormalMember, LargeMember, SmallMember, SmallMember] 3330
+  let s2 = calc [NormalMember, NormalMember, LargeMember, SmallMember] 3330
   print s2
 
-  let s3 = calc10yen 3 3330
+  let s3 = calc10yen [NormalMember, NormalMember, NormalMember, LargeMember, SmallMember, SmallMember] 3330
   print s3
